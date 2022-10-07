c4ccddbcc9e416e58e06ccd27126df678c209883
berscic <109431600+berscic@users.noreply.github.com>
2022-10-07 20:10:35 +0200

    Set-up state to alert search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <Dictionary />
     </div>
   );
 }
