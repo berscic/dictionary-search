b8859c6fa079cd69dc55df2bf3f2ed6604852108
berscic <109431600+berscic@users.noreply.github.com>
2022-10-08 16:23:30 +0200

    minor edits

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,12 +13,12 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
-
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
