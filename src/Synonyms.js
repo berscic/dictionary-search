d30c457f9b5bc8905562ee02c2d08e28cf5233f2
berscic <109431600+berscic@users.noreply.github.com>
2022-10-14 12:42:05 +0200

    synonyms edit

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul>
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
